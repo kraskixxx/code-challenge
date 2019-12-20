716478cdf21ab6950c061fd615a9c7e762d86900
kraskixxx <samet_163@hotmail.com>
2019-12-20 16:49:18 +0300

    MobileDeviceData alanlar ekleniyor.

--- src/main/java/com/budak/netaschallenge/domain/MobileDeviceData.java
+++ src/main/java/com/budak/netaschallenge/domain/MobileDeviceData.java
@@ -1,7 +1,11 @@
 package com.budak.netaschallenge.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * @author Samet BUDAK
@@ -10,5 +14,27 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "MOBILE_DEVICE_DATA")
 public class MobileDeviceData {
+
+    @GeneratedValue(generator = "modelDeviceData", strategy = GenerationType.AUTO)
+    @Id
+    @SequenceGenerator(name = "modelDeviceData", sequenceName = "MOBILE_DEVICE_DATA_ID_SEQ")
+    private Long id;
+
+    @Column
+    @NotEmpty
+    private String brand;
+
+    @Column
+    @NotEmpty
+    private String model;
+
+    @Column
+    @NotEmpty
+    private String os;
+
+    @Column
+    @NotEmpty
+    private String osVersion;
+
 
 }
