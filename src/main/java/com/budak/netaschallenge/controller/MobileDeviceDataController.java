1d1cfb212a721d65ef3e3eaef372c7c67e8ed64e
kraskixxx <samet_163@hotmail.com>
2019-12-20 21:36:22 +0300

    for requesting from Postman defined consumed MediaType

--- src/main/java/com/budak/netaschallenge/controller/MobileDeviceDataController.java
+++ src/main/java/com/budak/netaschallenge/controller/MobileDeviceDataController.java
@@ -3,6 +3,7 @@ package com.budak.netaschallenge.controller;
 import com.budak.netaschallenge.domain.MobileDeviceData;
 import com.budak.netaschallenge.service.MobileDeviceDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class MobileDeviceDataController {
     @Autowired
     MobileDeviceDataService mobileDeviceDataService;
 
-    @PostMapping("/saveMobileDeviceData")
+    @PostMapping(value = "/saveMobileDeviceData", consumes = MediaType.APPLICATION_JSON_VALUE)
     private Long saveMobileDeviceData(@Valid @RequestBody MobileDeviceData mobileDeviceData) {
         MobileDeviceData savedMobileDeviceData = mobileDeviceDataService.saveOrUpdate(mobileDeviceData);
         return savedMobileDeviceData.getId();
