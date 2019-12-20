40cf6731d72ea6b5c8db90f69f6c15de28508c3f
kraskixxx <samet_163@hotmail.com>
2019-12-20 17:38:34 +0300

    MobileDeviceDataController generated, added saveMobileDeviceData() method to "/saveMobileDeviceData" path

---
+++ src/main/java/com/budak/netaschallenge/controller/MobileDeviceDataController.java
@@ -0,0 +1,25 @@
+package com.budak.netaschallenge.controller;
+
+import com.budak.netaschallenge.domain.MobileDeviceData;
+import com.budak.netaschallenge.service.MobileDeviceDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Samet BUDAK
+ * @since
+ */
+@RestController
+public class MobileDeviceDataController {
+
+    @Autowired
+    MobileDeviceDataService mobileDeviceDataService;
+
+    @PostMapping("/saveMobileDeviceData")
+    private Long saveMobileDeviceData(@RequestBody MobileDeviceData mobileDeviceData) {
+        MobileDeviceData savedMobileDeviceData = mobileDeviceDataService.saveOrUpdate(mobileDeviceData);
+        return savedMobileDeviceData.getId();
+    }
+}
