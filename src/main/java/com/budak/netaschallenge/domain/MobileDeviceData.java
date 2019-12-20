a1a0e3e6fcde632b8179af3489e6621230eab8f5
kraskixxx <samet_163@hotmail.com>
2019-12-20 17:09:40 +0300

    getter setters and toString() method added, also @NotEmpty annotation added to "os" attribute

--- src/main/java/com/budak/netaschallenge/domain/MobileDeviceData.java
+++ src/main/java/com/budak/netaschallenge/domain/MobileDeviceData.java
@@ -28,11 +28,61 @@ public class MobileDeviceData {
 
     @Enumerated(EnumType.STRING)
     @Column
+    @NotEmpty
     private EnumOperatingSystem os;
 
     @Column
     @NotEmpty
     private String osVersion;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public EnumOperatingSystem getOs() {
+        return os;
+    }
+
+    public void setOs(EnumOperatingSystem os) {
+        this.os = os;
+    }
+
+    public String getOsVersion() {
+        return osVersion;
+    }
+
+    public void setOsVersion(String osVersion) {
+        this.osVersion = osVersion;
+    }
+
+    @Override
+    public String toString() {
+        return "MobileDeviceData{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", os=" + os +
+                ", osVersion='" + osVersion + '\'' +
+                '}';
+    }
 }
