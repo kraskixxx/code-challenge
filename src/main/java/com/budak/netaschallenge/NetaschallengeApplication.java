2668388c93020c25a1b480cbf17829703f8382b7
kraskixxx <samet_163@hotmail.com>
2019-12-20 22:21:17 +0300

    Swagger added for API documentation.

--- src/main/java/com/budak/netaschallenge/NetaschallengeApplication.java
+++ src/main/java/com/budak/netaschallenge/NetaschallengeApplication.java
@@ -13,6 +13,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,6 +25,7 @@ import java.util.List;
 import java.util.Set;
 
 @SpringBootApplication
+@EnableSwagger2
 public class NetaschallengeApplication {
 
 	public static void main(String[] args) {
