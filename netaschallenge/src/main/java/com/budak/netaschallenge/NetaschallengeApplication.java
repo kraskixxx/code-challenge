cf8b66c018d8977d537cf276da6d08f8bf7dc743
kraskixxx <samet_163@hotmail.com>
2019-12-20 16:00:45 +0300

    Yeni bir Spring Boot projesi oluşturuldu
    https://start.spring.io/ ile H2 Database, Spring Data JPA, Spring Boot DevTools içeren bir Spring Boot projesi oluşturuldu.

---
+++ netaschallenge/src/main/java/com/budak/netaschallenge/NetaschallengeApplication.java
@@ -0,0 +1,13 @@
+package com.budak.netaschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetaschallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetaschallengeApplication.class, args);
+	}
+
+}
---
+++ netaschallenge/src/test/java/com/budak/netaschallenge/NetaschallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budak.netaschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetaschallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
