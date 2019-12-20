a9b15c5eb09ed08e620dc211a595b2678b6684fd
kraskixxx <samet_163@hotmail.com>
2019-12-20 16:30:45 +0300

    MobileDeviceData adında domain sınıfı oluşturuldu

---
+++ src/main/java/com/budak/netaschallenge/domain/MobileDeviceData.java
@@ -0,0 +1,14 @@
+package com.budak.netaschallenge.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Samet BUDAK
+ * @since
+ */
+@Entity
+@Table(name = "MOBILE_DEVICE_DATA")
+public class MobileDeviceData {
+
+}
