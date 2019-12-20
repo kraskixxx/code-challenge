7ae78a929ebcb74abb17ea2fc425e8a3a49a344f
kraskixxx <samet_163@hotmail.com>
2019-12-20 17:53:45 +0300

    to read the JSON and write it to a database i use command line runner. first we read our domain model list and add all items in the List to HashSet because we dont want to add same data twice

--- src/main/java/com/budak/netaschallenge/NetaschallengeApplication.java
+++ src/main/java/com/budak/netaschallenge/NetaschallengeApplication.java
@@ -1,7 +1,19 @@
 package com.budak.netaschallenge;
 
+import com.budak.netaschallenge.domain.MobileDeviceData;
+import com.budak.netaschallenge.service.MobileDeviceDataService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 public class NetaschallengeApplication {
@@ -10,4 +22,25 @@ public class NetaschallengeApplication {
 		SpringApplication.run(NetaschallengeApplication.class, args);
 	}
 
+    @Bean
+    CommandLineRunner runner(MobileDeviceDataService mobileDeviceDataService) {
+        return args -> {
+            // read json and write to db
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<List<MobileDeviceData>> typeReference = new TypeReference<List<MobileDeviceData>>(){};
+            InputStream inputStream = TypeReference.class.getResourceAsStream("/devices.json");
+            try {
+                List<MobileDeviceData> mobileDeviceDataList = mapper.readValue(inputStream,typeReference);
+                Set<MobileDeviceData> mobileDeviceDataSet = new HashSet<>();
+                mobileDeviceDataSet.addAll(mobileDeviceDataList);
+
+                for (MobileDeviceData mobileDeviceData : mobileDeviceDataSet) {
+                    mobileDeviceDataService.saveOrUpdate(mobileDeviceData);
+                }
+                System.out.println("Mobile Device Datas are Saved!");
+            } catch (IOException e){
+                System.out.println("Unable to save Mobile Device Datas: " + e.getMessage());
+            }
+        };
+    }
 }
