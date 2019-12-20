26f91ccb3fb537701fb186153f5eb7008a55df7e
kraskixxx <samet_163@hotmail.com>
2019-12-20 19:00:39 +0300

    for resolving JSON errors added "SimpleModule" DeserializerModifier and Serializer because of different os Strings on devices.json file

--- src/main/java/com/budak/netaschallenge/NetaschallengeApplication.java
+++ src/main/java/com/budak/netaschallenge/NetaschallengeApplication.java
@@ -2,8 +2,13 @@ package com.budak.netaschallenge;
 
 import com.budak.netaschallenge.domain.MobileDeviceData;
 import com.budak.netaschallenge.service.MobileDeviceDataService;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,6 +32,32 @@ public class NetaschallengeApplication {
         return args -> {
             // read json and write to db
             ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
+
+            SimpleModule module = new SimpleModule();
+            module.setDeserializerModifier(new BeanDeserializerModifier() {
+                @Override
+                public JsonDeserializer<Enum> modifyEnumDeserializer(DeserializationConfig config,
+                                                                     final JavaType type,
+                                                                     BeanDescription beanDesc,
+                                                                     final JsonDeserializer<?> deserializer) {
+                    return new JsonDeserializer<Enum>() {
+                        @Override
+                        public Enum deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+                            Class<? extends Enum> rawClass = (Class<Enum<?>>) type.getRawClass();
+                            return Enum.valueOf(rawClass, jp.getValueAsString().toUpperCase());
+                        }
+                    };
+                }
+            });
+            module.addSerializer(Enum.class, new StdSerializer<Enum>(Enum.class) {
+                @Override
+                public void serialize(Enum value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+                    jgen.writeString(value.name().toLowerCase());
+                }
+            });
+            mapper.registerModule(module);
+
             TypeReference<List<MobileDeviceData>> typeReference = new TypeReference<List<MobileDeviceData>>(){};
             InputStream inputStream = TypeReference.class.getResourceAsStream("/devices.json");
             try {
